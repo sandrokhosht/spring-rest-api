b9284546eaa27adaa2313075b20d0f8a829f9e59
Aleksandre <sandrokhosht@gmail.com>
2020-10-12 12:03:32 +0400

    test

--- src/main/java/com/sandrokhosht/rest/api/SpringApplication.java
+++ src/main/java/com/sandrokhosht/rest/api/SpringApplication.java
@@ -8,5 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringApplication {
     public static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
-    }
+    }//
 }
